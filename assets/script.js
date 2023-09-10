88d61a3a59acc10307d95855dfdade7f9f799a76
RubabaKhandaker <140378374+RubabaKhandaker@users.noreply.github.com>
2023-09-10 21:57:12 +1000

    Added some preliminary code to script.js

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,14 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var generate = {
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowerCase: "abcdefghijklmnopqrstuvwxyz",
+  number: "0123456789",
+  symbol: "@#$%^&*()_+~|}{[]></-="
+}
+
 
 // Write password to the #password input
 function writePassword() {
