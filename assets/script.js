68228d79bcdf7bfa6955b729f3cf636a190ad64b
RubabaKhandaker <140378374+RubabaKhandaker@users.noreply.github.com>
2023-09-11 16:57:19 +1000

    Minor changes to javascript layout

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,20 @@
 // Assignment code here
 
-// Get references to the #generate element
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbol = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "~", "|", "}", "{", "[", "]", ">", "<", "/", "-", "="];
+
+// Gets references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var generate = {
-  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  lowerCase: "abcdefghijklmnopqrstuvwxyz",
-  number: "0123456789",
-  symbol: "@#$%^&*()_+~|}{[]></-="
+
+
+// Generates password based on given criteria
+function generatePassword() {
+
 }
 
-
-// Write password to the #password input
+// Writes password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -19,5 +23,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Adds event listener to generate button
 generateBtn.addEventListener("click", writePassword);
