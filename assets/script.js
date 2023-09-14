45d144670becc72ecb3376bc7ac781690c26ac07
RubabaKhandaker <140378374+RubabaKhandaker@users.noreply.github.com>
2023-09-14 21:56:34 +1000

    Added function that generates random passwords

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,27 @@
-// Assignment code here
+var passLength = 0; // 
+var applicableChars = ""; // stores selected character types
 
 // Gets references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Generates password based on given criteria
+// Function to randomly generate password based on given parameters 
 function generatePassword() {
+
+  applicableChars = ""; // resets every time invoked
+
+  passwordLength() > 0 ? passwordCharacterType() : invalidLengthAlert();
+  var result = "";
+
+  if( applicableChars != "" && passLength > 0 ){
+    var applicableCharsLength = applicableChars.length;
+    for ( var i = 0; i < passLength; i++ ) {
+       result += applicableChars.charAt(Math.floor(Math.random() * applicableCharsLength));
+    }
+
+    if( result != "" )
+      alert("A password has been generated for you successfully!");  
+  }
+  return result;
 
 }
 
