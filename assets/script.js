1428fef93493ac5ed95c46af18dc435f325b42a2
RubabaKhandaker <140378374+RubabaKhandaker@users.noreply.github.com>
2023-09-14 23:22:45 +1000

    Added functions to select character types to be included in generated password

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
-var passLength = 0; // 
-var applicableChars = ""; // stores selected character types
+var passLength = 0; // Stores password length
+var applicableChars = ""; // Stores selected character types
 
 // Gets references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -7,7 +7,7 @@ var generateBtn = document.querySelector("#generate");
 // Functions used to validate password length
 function passwordLength(){
   var lengthOfPass = prompt("Enter your preferred password length.");
-  lengthOfPass = parseInt(len);
+  lengthOfPass = parseInt(lengthOfPass);
   
   if(lengthOfPass < 8 || lengthOfPass > 128 || isNaN(lengthOfPass)) return 0; // If checks length of password
   
@@ -23,9 +23,9 @@ function invalidLength(){
 // Function to randomly generate password based on given parameters 
 function generatePassword() {
 
-  applicableChars = ""; // resets every time invoked
+  applicableChars = "";
 
-  passwordLength() > 0 ? passwordCharacterType() : invalidLengthAlert();
+  passwordLength() > 0 ? passwordChars() : invalidLength();
   var result = "";
 
   if( applicableChars != "" && passLength > 0 ){
@@ -39,6 +39,35 @@ function generatePassword() {
   }
   return result;
 
+}
+
+// Functions to select character types included in generated password
+
+function passwordChars(){
+  var upperCase =  confirm("Would you like to include uppercase characters? E.g. ABCDE");
+  var lowerCase =  confirm("Would you like to include lowercase characters? E.g. abcde");
+  var numbers =  confirm("Would you like to include numerical characters? E.g. 12345");
+  var specialChars =  confirm("Would you like to include special characters? E.g. !@#$%");
+
+  if( !upperCase && !lowerCase && !numbers && !specialChars ){
+    invalidCharacters();
+  } else {
+    if(upperCase)
+      applicableChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+    if(lowerCase)
+      applicableChars += "abcdefghijklmnopqrstuvwxyz";
+    
+    if(numbers)
+      applicableChars += "0123456789";
+
+    if(specialChars)
+      applicableChars += "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";  
+  }  
+}
+
+function invalidCharacters(){
+  alert("Uh oh! You must select OK to at least one of the character types prompted.");
 }
 
 // Writes password to the #password input
